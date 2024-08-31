571116dafb1b7bfb384628a37bd397250b3b1836
timothyvillaraza <61322637+timothyvillaraza@users.noreply.github.com>
2024-08-30 22:44:21 -0500

    Fixed warning

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,7 +4,14 @@ import './Home.css';
 const Home: React.FC = () => {
   return (
     <div className="home">
-      <iframe src="https://resume.creddle.io/embed/6x3f8thxdss" width="850" height="1100" seamless></iframe>
+        <iframe
+          src="https://resume.creddle.io/embed/6x3f8thxdss"
+          width="850"
+          height="1100"
+          className="home-iframe"
+          title="Resume of Timothy Villaraza"  // Add a descriptive title here
+          seamless
+      ></iframe>
     </div>
   );
 };
