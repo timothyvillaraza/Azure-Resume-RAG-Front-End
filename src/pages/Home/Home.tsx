3bca090187fa66e0a0e6db4344a9040f657e2f96
timothyvillaraza <61322637+timothyvillaraza@users.noreply.github.com>
2024-08-30 22:33:23 -0500

    Initial page layout and styling

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css'; 
+
+const Home: React.FC = () => {
+  return (
+    <div className="home">
+      <iframe src="https://resume.creddle.io/embed/6x3f8thxdss" width="850" height="1100" seamless></iframe>
+    </div>
+  );
+};
+
+export default Home;
